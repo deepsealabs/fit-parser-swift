2e3598ce366410df519c80a49ee9701a5f7d8ce9
latishab <latishabesariani@gmail.com>
2024-08-05 09:26:55 +0800

    Initial Commit

---
+++ FIT-Parser/FIT_ParserApp.swift
@@ -0,0 +1,17 @@
+//
+//  FIT_ParserApp.swift
+//  FIT-Parser
+//
+//  Created by Latisha Besariani on 05/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FIT_ParserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
