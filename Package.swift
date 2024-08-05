d031f79366c36e66115d2099872b375a45facfdc
latishab <latishabesariani@gmail.com>
2024-08-05 12:53:39 +0800

    update: sdk versioning

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "FITParser",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v10_15)
+        .iOS(.v14),
+        .macOS(.v11)
     ],
     products: [
         .library(
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["FITParser"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/garmin/fit-objective-c-sdk.git")
+        .package(url: "https://github.com/garmin/fit-objective-c-sdk.git", from: "21.141.0")
     ],
     targets: [
         .target(
